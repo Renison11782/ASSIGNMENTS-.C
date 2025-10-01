9caaf97bd582a564eaec033b9f748b2b6acefa86
Renison11782 <renisonrobinson@gmail.com>
2025-10-02 00:00:55 +0300

    Add files via upload

---
+++ python program.c
@@ -0,0 +1,16 @@
+/*
+name:Robinson Renison
+reg:CT100/G/26162/25
+des:implementing bank loan requirements
+*/
+
+ print("Plase enter your age:");
+ age = int(input())
+ 
+ print("please enter your annual income:")
+ income = float(input())
+ 
+ if age >= 21 and income>= 21000:
+ 	print("congratulations youqualify for a loan.")
+ elif age < 21 or income ,21000:
+    print("Unfortunately, we are unable to offer you the loan.") 
