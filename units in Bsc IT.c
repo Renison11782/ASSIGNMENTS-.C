5ebff62f925dbb769d5bffa6f3843195111a77b5
Renison11782 <renisonrobinson@gmail.com>
2025-10-01 23:41:30 +0300

    Add files via upload

---
+++ units in Bsc IT.c
@@ -0,0 +1,24 @@
+/*
+name:Robinson Renison
+reg:CT100/G/26162/25
+des:Bsc Information Technology Units
+*/
+
+#include <stdio.h>
+//main function 
+int main()
+{
+	printf("units under information technology:\n");
+	printf("semester one eight units in total:\n");
+	printf("analogue electronics:\n");
+	printf("essentials of accounting:\n");
+	printf("introduction to computer applications:\n");
+	printf("computer systems and organizations:\n");
+	printf("fundamentals of ict:\n");
+	printf("communication skills:\n");
+	printf("mathematics for science:\n");
+	printf("introduction to programming and algorithms:\n");
+	printf("a total of eight units for group A in semester one");
+	
+	return 0;
+}
