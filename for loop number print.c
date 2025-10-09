1922fa64d2f7e4fa4d7b36ec32d56ceba355769d
Renison11782 <renisonrobinson@gmail.com>
2025-10-09 10:20:10 +0300

    Add files via upload

---
+++ for loop number print.c
@@ -0,0 +1,15 @@
+/*
+name: Robinson Renison
+reg: CT100/G/26162/25
+DES:for loop counting 100 to 50 in descending order
+*/
+# include <stdio.h>
+int main(){
+int i;
+
+
+for( i=100; i>=50; i--){
+	printf("%d \n", i);
+}	
+return 0;
+}
